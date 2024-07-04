c64dc997b3488499a6328c4c671a3065878288be
yedu <mailyeduraghav@gmail.com>
2024-07-04 17:31:24 -0400

    Fixed dependency specification

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,9 @@
 rootProject.name = "sdkconsumer1"
 include(":app")
 include(":testsdk1:sdk")
+include(":testsdk1:domain")
+include(":testsdk1:web")
+
 project(":testsdk1:sdk").projectDir = File("testsdk1/sdk")
+project(":testsdk1:domain").projectDir = File("testsdk1/domain")
+project(":testsdk1:web").projectDir = File("testsdk1/web")
